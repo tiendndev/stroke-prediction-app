e2e3f0970fea1b768109431570f2dd4841fca031
tiendndev <birongze300401@gmail.com>
2023-04-20 18:51:54 +0700

    separate host constant

--- strokeprediction/static/api.js
+++ strokeprediction/static/api.js
@@ -1,18 +1,4 @@
-const VIEW_NGROK_API =
-   "https://0dc3-1-52-161-65.ngrok-free.app/patient-management/views";
-const DELETE_NGROK_API =
-   "https://0dc3-1-52-161-65.ngrok-free.app/patient-management/delete";
-const PREDICT_NGROK_API = "";
+const NGROK_HOST = "https://0dc3-1-52-161-65.ngrok-free.app";
+const LOCAL_HOST = "http://127.0.0.1:5000";
 
-const VIEW_LOCAL_API = "http://127.0.0.1:5000/patient-management/views";
-const DELETE_LOCAL_API = "http://127.0.0.1:5000/patient-management/delete";
-const PREDICT_LOCAL_API = "http://127.0.0.1:5000/image";
-
-export {
-   VIEW_NGROK_API,
-   DELETE_NGROK_API,
-   PREDICT_NGROK_API,
-   VIEW_LOCAL_API,
-   DELETE_LOCAL_API,
-   PREDICT_LOCAL_API,
-};
+export { NGROK_HOST, LOCAL_HOST };
