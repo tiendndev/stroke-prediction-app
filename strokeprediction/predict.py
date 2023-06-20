ff2521e372f287a31d154bf5726a74336776ed90
tiendndev <birongze300401@gmail.com>
2023-06-20 14:50:45 +0700

    deploy on render v17

--- strokeprediction/predict.py
+++ strokeprediction/predict.py
@@ -2,12 +2,14 @@ from flask import request
 from keras.utils import load_img, img_to_array
 from keras.models import load_model
 import numpy as np
+import tensorflow as tf
 
 
 def prediction_model():
     classes = ['Normal', 'Stroke']
-    model_path = "./strokeprediction/h5/model.h5"
-    new_model = load_model(model_path)
+    model_path = "./strokeprediction/h5/model.tflite"
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
 
     img = request.files["file"]
     image_path = "./strokeprediction/static/img/img" + img.filename
@@ -17,11 +19,16 @@ def prediction_model():
     test_image = img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
 
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], test_image)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
     print("img_url: ", image_path[26:])
 
-    result = new_model.predict(test_image)
-    result1 = result[0]
-    for y in range(2):
-        if result1[y] == 1.:
-            break
-    return [classes[y], image_path[26:]]
+    result = np.argmax(output_data)
+    prediction = classes[result]
+
+    return [prediction, image_path[26:]]
